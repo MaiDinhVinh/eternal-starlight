4649614431c3a6ca1d3e55f4b453cf61253eb91f
Leo Lezury <68064905+LeoLezury@users.noreply.github.com>
2025-03-12 22:09:52 +0800

    feat: more book content & unlock toast

--- common/src/main/java/cn/leolezury/eternalstarlight/common/client/gui/screen/BookScreen.java
+++ common/src/main/java/cn/leolezury/eternalstarlight/common/client/gui/screen/BookScreen.java
@@ -4,6 +4,8 @@ import cn.leolezury.eternalstarlight.common.client.book.Book;
 import cn.leolezury.eternalstarlight.common.client.book.BookAccess;
 import cn.leolezury.eternalstarlight.common.client.book.component.BookComponentDefinition;
 import cn.leolezury.eternalstarlight.common.client.gui.screen.widget.ESPageButton;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.RenderType;
@@ -12,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
+@Environment(EnvType.CLIENT)
 public class BookScreen extends Screen {
 	private final Book book;
 	private int pageSize;
--- common/src/main/java/cn/leolezury/eternalstarlight/common/client/shader/ESShaders.java
+++ common/src/main/java/cn/leolezury/eternalstarlight/common/client/shader/ESShaders.java
@@ -4,7 +4,11 @@ import cn.leolezury.eternalstarlight.common.EternalStarlight;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import net.minecraft.client.renderer.ShaderDefines;
 import net.minecraft.client.renderer.ShaderProgram;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.ShaderInstance;
 
+@Environment(EnvType.CLIENT)
 public class ESShaders {
 	public static final ShaderProgram RENDERTYPE_GUI_CREST_SELECTION = new ShaderProgram(EternalStarlight.id("core/rendertype_gui_crest_selection"), DefaultVertexFormat.POSITION_TEX, ShaderDefines.EMPTY);
 	public static final ShaderProgram RENDERTYPE_STARLIGHT_PORTAL = new ShaderProgram(EternalStarlight.id("core/rendertype_starlight_portal"), DefaultVertexFormat.BLOCK, ShaderDefines.EMPTY);
