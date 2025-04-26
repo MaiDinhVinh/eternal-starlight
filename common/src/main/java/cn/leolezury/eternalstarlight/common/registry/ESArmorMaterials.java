772e5a40033dd3a447af9483c3cdfbd02a555e7c
Leo Lezury <68064905+LeoLezury@users.noreply.github.com>
2025-04-26 20:03:32 +0800

    feat: stranghouls' spear & bow attack

--- common/src/main/java/cn/leolezury/eternalstarlight/common/registry/ESArmorMaterials.java
+++ common/src/main/java/cn/leolezury/eternalstarlight/common/registry/ESArmorMaterials.java
@@ -17,6 +17,7 @@ import net.minecraft.world.item.equipment.ArmorType;
 import java.util.EnumMap;
 import java.util.List;
 
+// TODO: FIXME
 public class ESArmorMaterials {
 	public static final RegistrationProvider<ArmorMaterial> ARMOR_MATERIALS = RegistrationProvider.get(Registries.ARMOR_MATERIAL, EternalStarlight.ID);
 	public static final RegistryObject<ArmorMaterial, ArmorMaterial> ALCHEMIST = ARMOR_MATERIALS.register("alchemist", () -> new ArmorMaterial(10, Util.make(new EnumMap<>(ArmorType.class), (map) -> {
@@ -54,12 +55,54 @@ public class ESArmorMaterials {
 		map.put(ArmorType.LEGGINGS, 5);
 		map.put(ArmorType.CHESTPLATE, 6);
 		map.put(ArmorType.HELMET, 2);
+	public static final RegistryObject<ArmorMaterial, ArmorMaterial> ALCHEMIST = ARMOR_MATERIALS.register("alchemist", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+		map.put(ArmorItem.Type.BOOTS, 2);
+		map.put(ArmorItem.Type.LEGGINGS, 5);
+		map.put(ArmorItem.Type.CHESTPLATE, 6);
+		map.put(ArmorItem.Type.HELMET, 2);
+		map.put(ArmorItem.Type.BODY, 5);
+	}), 15, ESSoundEvents.ARMOR_EQUIP_ALCHEMIST.asHolder(), () -> Ingredient.of(ESConventionalTags.Items.GEMS_THIOQUARTZ), List.of(new ArmorMaterial.Layer(EternalStarlight.id("alchemist"))), 0F, 0F));
+	public static final RegistryObject<ArmorMaterial, ArmorMaterial> AMARAMBER = ARMOR_MATERIALS.register("amaramber", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+		map.put(ArmorItem.Type.BOOTS, 2);
+		map.put(ArmorItem.Type.LEGGINGS, 5);
+		map.put(ArmorItem.Type.CHESTPLATE, 5);
+		map.put(ArmorItem.Type.HELMET, 2);
+		map.put(ArmorItem.Type.BODY, 5);
+	}), 9, ESSoundEvents.ARMOR_EQUIP_AMARAMBER.asHolder(), () -> Ingredient.of(ESConventionalTags.Items.INGOTS_AMARAMBER), List.of(new ArmorMaterial.Layer(EternalStarlight.id("amaramber"))), 0F, 0F));
+	public static final RegistryObject<ArmorMaterial, ArmorMaterial> AETHERSENT = ARMOR_MATERIALS.register("aethersent", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+		map.put(ArmorItem.Type.BOOTS, 3);
+		map.put(ArmorItem.Type.LEGGINGS, 6);
+		map.put(ArmorItem.Type.CHESTPLATE, 7);
+		map.put(ArmorItem.Type.HELMET, 3);
+		map.put(ArmorItem.Type.BODY, 11);
+	}), 35, ESSoundEvents.ARMOR_EQUIP_AETHERSENT.asHolder(), () -> Ingredient.of(ESConventionalTags.Items.INGOTS_AETHERSENT), List.of(new ArmorMaterial.Layer(EternalStarlight.id("aethersent"))), 0F, 0F));
+	public static final RegistryObject<ArmorMaterial, ArmorMaterial> THERMAL_SPRINGSTONE = ARMOR_MATERIALS.register("thermal_springstone", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+		map.put(ArmorItem.Type.BOOTS, 2);
+		map.put(ArmorItem.Type.LEGGINGS, 5);
+		map.put(ArmorItem.Type.CHESTPLATE, 6);
+		map.put(ArmorItem.Type.HELMET, 3);
+		map.put(ArmorItem.Type.BODY, 7);
+	}), 15, ESSoundEvents.ARMOR_EQUIP_THERMAL_SPRINGSTONE.asHolder(), () -> Ingredient.of(ESConventionalTags.Items.INGOTS_THERMAL_SPRINGSTONE), List.of(new ArmorMaterial.Layer(EternalStarlight.id("thermal_springstone"))), 0.5F, 0F));
+	public static final RegistryObject<ArmorMaterial, ArmorMaterial> GLACITE = ARMOR_MATERIALS.register("glacite", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+		map.put(ArmorItem.Type.BOOTS, 3);
+		map.put(ArmorItem.Type.LEGGINGS, 5);
+		map.put(ArmorItem.Type.CHESTPLATE, 6);
+		map.put(ArmorItem.Type.HELMET, 3);
+		map.put(ArmorItem.Type.BODY, 7);
+	}), 9, ESSoundEvents.ARMOR_EQUIP_GLACITE.asHolder(), () -> Ingredient.of(ESConventionalTags.Items.GEMS_GLACITE), List.of(new ArmorMaterial.Layer(EternalStarlight.id("glacite"))), 0.5F, 0F));
+	public static final RegistryObject<ArmorMaterial, ArmorMaterial> SWAMP_SILVER = ARMOR_MATERIALS.register("swamp_silver", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
+		map.put(ArmorItem.Type.BOOTS, 2);
+		map.put(ArmorItem.Type.LEGGINGS, 5);
+		map.put(ArmorItem.Type.CHESTPLATE, 6);
+		map.put(ArmorItem.Type.HELMET, 2);
+		map.put(ArmorItem.Type.BODY, 5);
 	}), 25, ESSoundEvents.ARMOR_EQUIP_SWAMP_SILVER.asHolder(), () -> Ingredient.of(ESConventionalTags.Items.INGOTS_SWAMP_SILVER), List.of(new ArmorMaterial.Layer(EternalStarlight.id("swamp_silver"))), 0.5F, 0.2F));
 	public static final RegistryObject<ArmorMaterial, ArmorMaterial> AIR_SAC = ARMOR_MATERIALS.register("air_sac", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
 		map.put(ArmorItem.Type.BOOTS, 1);
 		map.put(ArmorItem.Type.LEGGINGS, 4);
 		map.put(ArmorItem.Type.CHESTPLATE, 4);
 		map.put(ArmorItem.Type.HELMET, 1);
+		map.put(ArmorItem.Type.BODY, 4);
 	}), 9, SoundEvents.ARMOR_EQUIP_LEATHER, () -> Ingredient.of(ESItems.TOWER_SQUID_AIR_SAC.get()), List.of(new ArmorMaterial.Layer(EternalStarlight.id("air_sac"))), 0F, 0F));
 
 	public static void loadClass() {
